62257a2ab7002370a8983b8265789a166d66d86f
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-11 14:43:18 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetRESTClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
