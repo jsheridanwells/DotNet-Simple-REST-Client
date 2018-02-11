0271dcb711f04d3aab47926c05dd6dbd94d86a44
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-11 14:55:20 -0600

    makes simple api get request to github

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace DotNetRESTClient
 {
     class Program
     {
-    static void Main(string[] args) => ProcessRepositories().Wait();
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args) => ProcessRepositories().Wait();
 
-    private static async Task ProcessRepositories()
-        {
+        private static async Task ProcessRepositories()
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")
+                );
+                client.DefaultRequestHeaders.Add(
+                    "User-Agent", ".NET Foundation Repository Reporter"
+                );
 
-        }
+                var taskString = client.GetStringAsync(
+                    "https://api.github.com/orgs/dotnet/repos"
+                );
+
+                var message = await taskString;
+                Console.WriteLine(message);
+            }
     }
 }
